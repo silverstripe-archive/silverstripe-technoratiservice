5f99afddd0d9d023a281552ef68dc0cc8a896276
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-11-02 20:12:11 +0000

    MINOR Updated master language tables
    git-svn-id: http://svn.silverstripe.com/open/modules/technoratiservice/trunk@65087 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- lang/en_US.php
+++ lang/en_US.php
@@ -12,25 +12,5 @@ $lang['en_US']['MetaBlog']['SINGULARNAME'] = array(
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
-$lang['en_US']['MetaBlog']['db_ExcerptSize'] = array(
-	'ExcerptSize',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['MetaBlog']['db_PerPage'] = array(
-	'PerPage',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['MetaBlog']['db_Tag'] = array(
-	'Tag',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['MetaBlog']['db_TopExcerptSize'] = array(
-	'TopExcerptSize',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
 
 ?>
